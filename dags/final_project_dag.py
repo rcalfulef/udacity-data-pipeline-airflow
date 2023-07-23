d7d3d39427744de1b8818df605f0ab5d0ef0f8c3
Andrés Calfulef <rcalfulef@gmail.com>
2023-07-23 18:57:57 -0400

    add defaults args

--- dags/final_project_dag.py
+++ dags/final_project_dag.py
@@ -18,6 +18,7 @@ from helpers.sql_queries import SqlQueries
 
 default_args = {
     "owner": "Andrés Calfulef",
+    "depends_on_past": False,
     "start_date": datetime.now(),
     "retries": 3,
     "retry_delay": timedelta(minutes=5),
@@ -29,7 +30,7 @@ default_args = {
 @dag(
     default_args=default_args,
     description="Load and transform data in Redshift with Airflow",
-    schedule_interval="0 * * * *",
+    schedule_interval="@hourly",
 )
 def final_project():
     """Final project dag"""
